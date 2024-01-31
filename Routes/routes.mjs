dcbdab2acb7a2cd1702a024984d4bd5625bb47f0
jokatty <jyotikattani@gmail.com>
2024-01-31 13:12:15 +0100

    add register and login routes

--- Routes/routes.mjs
+++ Routes/routes.mjs
@@ -4,4 +4,10 @@ import initIndexController from '../controller/index.mjs';
 export default function routes(app) {
   const indexController = initIndexController();
   app.get('/', indexController.index);
+  app.get('/register', (req, res) => {
+    res.render('register');
+  });
+  app.get('/login', (req, res) => {
+    res.render('login');
+  });
 }
