f71d54034468e730067f37ddf9573c6f55c1c6d2
jokatty <jyotikattani@gmail.com>
2024-01-31 13:40:51 +0100

    add register user post route

--- Routes/routes.mjs
+++ Routes/routes.mjs
@@ -1,8 +1,10 @@
 import db from '../db/models/index.mjs';
 import initIndexController from '../controller/index.mjs';
+import initUserRegistration from '../controller/register.mjs';
 
 export default function routes(app) {
   const indexController = initIndexController();
+  const userRegistrationController = initUserRegistration(db);
   app.get('/', indexController.index);
   app.get('/register', (req, res) => {
     res.render('register');
@@ -10,4 +12,5 @@ export default function routes(app) {
   app.get('/login', (req, res) => {
     res.render('login');
   });
+  app.post('/register', userRegistrationController.createUser);
 }
