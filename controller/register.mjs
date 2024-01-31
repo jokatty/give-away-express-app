e4c10846358b90db12f61df6796473daabcbaa44
jokatty <jyotikattani@gmail.com>
2024-01-31 13:39:51 +0100

    add create user controller

---
+++ controller/register.mjs
@@ -0,0 +1,36 @@
+import jsSHA from 'jssha';
+
+export default function initUserRegistration(db) {
+  /**
+ * callback function for register post route. fetch user form data and save in db.
+ * @param {string} req - route's request.
+ * @param {string} res - route's response.
+ */
+  const createUser = async (req, res) => {
+    const { fname, lname, email } = req.body;
+    console.log(fname + lname + email);
+    // hash user password
+    const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
+    shaObj.update(req.body.pwd);
+    const hashedPassword = shaObj.getHash('HEX');
+    try {
+      const newUser = await db.User.create({
+        first_name: fname,
+        last_name: lname,
+        email,
+        password: hashedPassword,
+      });
+      res.cookie('isLoggedIn', 'true');
+      res.cookie('userName', `${fname}`);
+      res.cookie('userId', `${newUser.id}`);
+      res.redirect('/listing');
+      res.send('User created');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return {
+    createUser,
+  };
+}
