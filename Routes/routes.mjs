afad7ed04944e126305d0b9b70f960908ebe3617
jokatty <jyotikattani@gmail.com>
2024-01-31 13:09:31 +0100

    add index route

--- Routes/routes.mjs
+++ Routes/routes.mjs
@@ -1,6 +1,7 @@
 import db from '../db/models/index.mjs';
+import initIndexController from '../controller/index.mjs';
 
-
-export default function routes(app){
-  
+export default function routes(app) {
+  const indexController = initIndexController();
+  app.get('/', indexController.index);
 }
