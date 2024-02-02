1c3eb4016b7ad1f53df3cddf58984c8e586cef5e
jokatty <jyotikattani@gmail.com>
2024-02-02 11:49:47 +0100

    add listing get route

--- Routes/routes.mjs
+++ Routes/routes.mjs
@@ -2,11 +2,13 @@ import db from '../db/models/index.mjs';
 import initIndexController from '../controller/index.mjs';
 import initUserRegistration from '../controller/register.mjs';
 import initLoginController from '../controller/login.mjs';
+import initListingController from '../controller/listing.mjs';
 
 export default function routes(app) {
   const indexController = initIndexController();
   const userRegistrationController = initUserRegistration(db);
   const loginController = initLoginController(db);
+  const listingController = initListingController(db);
 
   app.get('/', indexController.index);
   app.get('/register', (req, res) => {
@@ -17,4 +19,6 @@ export default function routes(app) {
   });
   app.post('/register', userRegistrationController.createUser);
   app.post('/login', loginController.handleLogin);
+  // routes related to listing
+  app.get('/listing', listingController.allowListing);
 }
