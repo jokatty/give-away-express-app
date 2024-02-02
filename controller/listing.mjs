ea630f3287569aa00165642a94771c8c57422bd4
jokatty <jyotikattani@gmail.com>
2024-02-02 12:14:03 +0100

    add display category page controller

--- controller/listing.mjs
+++ controller/listing.mjs
@@ -12,7 +12,39 @@ export default function initListingController(db) {
       res.render('guest-user');
     }
   };
+
+  /**
+ * callback function to render category page.
+ * checks for user login status and sets the nav
+ * queries the 'listings' table and fetches products of the the requested categoy.
+ */
+  const displayCategoryPage = async (req, res) => {
+    let nav = '';
+    const { userName } = req.cookies;
+    if (userName) {
+      nav = 'index-loggedin-nav';
+    } else {
+      nav = 'index-nav';
+    }
+    // extract params data passed in the route
+    const categoryName = req.params.category;
+    const category = `${categoryName[0].toUpperCase()}${categoryName.slice(1)}`;
+    try {
+      const listings = await db.Listing.findAll({
+        where: {
+          product_category: categoryName,
+        },
+      });
+      res.render('category', {
+        productInfo: listings, category, nav, userName,
+      });
+    } catch (err) {
+      console.log(err.stack);
+      res.status(500).send('Internal Server Error');
+    }
+  };
   return {
     allowListing,
+    displayCategoryPage,
   };
 }
