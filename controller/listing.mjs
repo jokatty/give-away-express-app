4982aa7653f845dbd6372325b94d2c7d1c46e64b
jokatty <jyotikattani@gmail.com>
2024-02-03 14:03:45 +0100

    add create listinglogic

--- controller/listing.mjs
+++ controller/listing.mjs
@@ -43,8 +43,45 @@ export default function initListingController(db) {
       res.status(500).send('Internal Server Error');
     }
   };
+
+  const createListing = async (req, res) => {
+    /**
+ * callback function for '/listing' post route.
+ * update the listings table with the user input data.
+ * use 'multer' for storing user generated data in uploads dir.
+ */
+    console.log('request came in');
+    console.log(req.file);
+    const {
+      productCategory, productName, productDescription,
+    } = req.body;
+    const { userId } = req.cookies;
+    //  insert the new listing to the listing table
+    const newListing = await db.Listing.create({
+      productCategory,
+      productName,
+      productDescription,
+      productImageInfo: req.file.filename,
+      userId,
+      isAvailable: true,
+    });
+
+    // query the listing table for all listing where category is this particular category
+    // TODO: this redirect logic needs to completed.
+    const categoryListing = await db.Listing.findAll({
+      where: {
+        product_category: productCategory,
+      },
+    });
+    console.log(categoryListing);
+    const category = `${productCategory[0].toUpperCase()}${productCategory.slice(1)}`;
+    // to direct user to the product category page they posted
+    // res.render('category', { productInfo: categoryListing, category });
+    res.redirect('/dashboard/added-product');
+  };
   return {
     allowListing,
     displayCategoryPage,
+    createListing,
   };
 }
