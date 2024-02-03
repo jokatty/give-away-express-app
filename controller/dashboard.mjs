c3f8798babe5fbe2d1f1d280ced5470a68cd20ef
jokatty <jyotikattani@gmail.com>
2024-02-03 15:00:24 +0100

    add user dashboard

---
+++ controller/dashboard.mjs
@@ -0,0 +1,45 @@
+export default function initDashboardController(db) {
+  /**
+ * callback function for '/dashboard' route.
+ * renders user's dashboard with the all transactions history.
+ * quries listings table and requests table.
+ */
+  const renderUserDashboard = async (req, res) => {
+    const { userId } = req.cookies;
+    let nav = '';
+    const { userName } = req.cookies;
+    if (userName) {
+      nav = 'index-loggedin-nav';
+    } else {
+      nav = 'index-nav';
+    }
+    try {
+      // select all the listing made by the user with userId
+      const userListings = await db.Listing.findAll({
+        where: {
+          user_id: userId,
+        },
+      });
+      // select all the listing requested by the user with userId
+      const userRequests = await db.Listing.findAll({
+        include: [
+          {
+            model: db.Request,
+            where: {
+              user_id: userId,
+            },
+          },
+        ],
+      });
+      res.render('dashboard-user', {
+        listedProducts: userListings, requestedProducts: userRequests, nav, userName,
+      });
+    } catch (err) {
+      console.log(err.stack);
+      res.status(500).send('Internal Server Error');
+    }
+  };
+  return {
+    renderUserDashboard,
+  };
+}
